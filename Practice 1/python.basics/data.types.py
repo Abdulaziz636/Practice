2c885f5149b14eddffe39ed1b28a39b5d8b86693
Abdulaziz636 <qkiller912@gmail.com>
2026-02-02 19:25:29 +0500

    data.types is done.

--- Practice 1/python.basics/data.types.py
+++ Practice 1/python.basics/data.types.py
@@ -1,2 +1,40 @@
 x = 5
 print(type(x))
+
+x = "Hello World"
+print(type(x))
+
+x = 20
+print(type(x))
+
+x = 20.5
+print(type(x))
+
+
+x = 1j
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+x = ("apple", "banana", "cherry")	#tuple	
+
+x = range(6)	#range	
+
+x = {"name" : "John", "age" : 36}	#dict	
+
+x = {"apple", "banana", "cherry"}	#set	
+
+x = frozenset({"apple", "banana", "cherry"})	#frozenset	
+
+x = True	#bool	
+
+x = b"Hello"	#bytes	
+
+x = bytearray(5)	 #bytearray	
+
+x = memoryview(bytes(5))	#memoryview	
+
+x = None
+
+print(type(x))
